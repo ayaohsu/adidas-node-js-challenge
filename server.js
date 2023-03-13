40df93b8668fe20334314517a31a393e7587c082
Tsung-Yao Hsu <ayao780607@gmail.com>
2023-03-13 17:10:47 +0800

    subscribe request on public service

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
-// Constants
 const PORT = 8080;
 
-// App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+
+
+app.post('/subscribe', (req, res) => {
+    console.log(`Received subscribe request. [body=${JSON.stringify(req.body)}]`)
+    
+    requiredKeys = ['firstName','lastName', 'email']
+    for (requiredKey of requiredKeys){
+        if (!(requiredKey in req.body)){
+            res.status(400).send(`Missing required key ${requiredKey}.`);
+            return;
+        }
+    }
+    res.status(201).send();
+})
+
 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
