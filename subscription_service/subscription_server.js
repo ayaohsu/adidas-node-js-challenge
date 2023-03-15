8f5c3a590e7ed50e26ce0c87170664f7aa8e9832
Tsung-Yao Hsu <ayao780607@gmail.com>
2023-03-15 12:08:27 +0800

    Add subscription service

---
+++ subscription_service/subscription_server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = 8081;
+
+const app = express();
+app.use(express.json());
+
+
+app.post('/subscribe', (req, res) => {
+    console.log(`Received subscribe request. [body=${JSON.stringify(req.body)}]`)
+
+    res.status(201).send();
+})
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
