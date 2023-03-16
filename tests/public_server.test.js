9d788fc257c68c175bc61439c7943a9ac6fecc88
Tsung-Yao Hsu <ayao780607@gmail.com>
2023-03-17 01:53:29 +0800

    comment out unit test

--- tests/public_server.test.js
+++ tests/public_server.test.js
@@ -1,25 +1,27 @@
-// const handleSubscribeRequest = require('../public_service/public_server.js');
+/*
+const handleSubscribeRequest = require('../public_service/public_server.js');
 
-// import mockAxios from 'jest-mock-axios';
-// FIXME: The above failed with 'SyntaxError: Cannot use import statement outside a module at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1495:14)'
+import mockAxios from 'jest-mock-axios';
+FIXME: The above failed with 'SyntaxError: Cannot use import statement outside a module at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1495:14)'
 
-// afterEach(() => {
-//     mockAxios.reset();
-// });
+afterEach(() => {
+    mockAxios.reset();
+});
 
-// test('Success response with status 201 should be returned to valid subscription requests', () => {
-//     const request = {
-//         'body':{
-//             'firstName': 'Bob',
-//             'lastName': 'Adams',
-//             'email': 'bob.adams@123.com'
-//         }
-//     };
-//     let mockedResponse = mockAxios.mockResponse;
+test('Success response with status 201 should be returned to valid subscription requests', () => {
+    const request = {
+        'body':{
+            'firstName': 'Bob',
+            'lastName': 'Adams',
+            'email': 'bob.adams@123.com'
+        }
+    };
+    let mockedResponse = mockAxios.mockResponse;
     
-//     handleSubscribeRequest(request, mockedResponse);
+    handleSubscribeRequest(request, mockedResponse);
     
-//     expect(mockAxios.post).toHaveBeenCalledWith('http://subscription_service:8081/subscribe', request);
+    expect(mockAxios.post).toHaveBeenCalledWith('http://subscription_service:8081/subscribe', request);
 
-//     expect(mockedResponse.status).toEqual(201);
-// });
+    expect(mockedResponse.status).toEqual(201);
+});
+*/
